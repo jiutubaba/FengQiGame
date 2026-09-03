e8ad730d924cf24d608db1a74a031bf69fb5fee8
风起 <1334991711@qq.com>
2026-09-04 00:34:54 +0800

    test: exclude release worktrees from discovery

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   envDir: false,
   plugins: [react()],
+  test: {
+    exclude: ["**/node_modules/**", "**/.git/**", "**/.runtime/**"],
+  },
   server: {
     host: "127.0.0.1",
     open: true,
